f0dde83940154725fab022280e34f531dfedcb6a
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-09-24 01:03:58 +0200

    feat(git): retrieve git origin, diff and output are printed in grey

--- src/git.rs
+++ src/git.rs
@@ -1,10 +1,27 @@
-use std::{io::Error, process::{Command, Output}};
+use std::process::{Command, Output};
+use colored::Colorize;
 
-// takes an array of &str and pass them as arg
-pub fn git (args: &[&str]) -> Result<Output, Error> {
-  println!("+> git {}", args.join(" "));
+pub fn git (args: &[&str]) -> Output {
+  let log = format!("+> git {}", args.join(" "));
+  println!("{}", log.bright_black());
 
   Command::new("git")
     .args(args)
     .output()
+    .expect("Failed to run git command, make sure git is installed on your machine.")
+}
+
+// get every commit message between the two versions
+pub fn diff (branch_name: &str, old_version: &str, new_version: &str) -> String {
+  let references = format!("{branch_name}-v{old_version}..{branch_name}-v{new_version}");
+
+  let output = git(&["log", "--oneline", "--pretty=format:%s (%h)", &references]);
+  String::from_utf8_lossy(&output.stdout).to_string()
+}
+
+pub fn origin_url () -> String {
+  let output = git(&["remote", "get-url", "origin"]);
+  let url = String::from_utf8_lossy(&output.stdout);
+
+  url.trim().to_string()
 }
